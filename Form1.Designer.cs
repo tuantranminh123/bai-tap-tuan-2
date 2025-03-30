e78e206bcb9f700942d123e53eea96bfe5e236e6
TMT0103 <tranminhtuan010305@gmail.com>
2025-03-30 15:36:58 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace bài_tập_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFolder = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFolder
+            // 
+            this.btnLoadFolder.Location = new System.Drawing.Point(23, 49);
+            this.btnLoadFolder.Name = "btnLoadFolder";
+            this.btnLoadFolder.Size = new System.Drawing.Size(101, 42);
+            this.btnLoadFolder.TabIndex = 0;
+            this.btnLoadFolder.Text = "&Load Folder";
+            this.btnLoadFolder.UseVisualStyleBackColor = true;
+            this.btnLoadFolder.Click += new System.EventHandler(this.btnLoadFolder_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 140);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+           
+            // 
+            // lblFile
+            // 
+            this.lblFile.Location = new System.Drawing.Point(-3, 406);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(138, 23);
+            this.lblFile.TabIndex = 2;
+            this.lblFile.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 267);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(796, 100);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLoadFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bài_tập_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+                string[] files =Directory.GetFiles(path);
+                foreach (var file in files)
+                {
+                    if (file.EndsWith(".jpg")||file.EndsWith(".png"))
+                    {
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Cursor = Cursors.Hand;
+                        pictureBox.Load(file);
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox.Tag = file;
+                        pictureBox.Click+= PictureBox_Click;
+                        flowLayoutPanel1.Controls.Add(pictureBox);
+                    }
+                }
+            }
+        }
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                string imgPath = pictureBox.Tag.ToString(); 
+                pictureBox1.Load(imgPath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                lblFile.Text = "File "+ imgPath + " is loaded.";
+            }
+        }
+
+    }
+}
